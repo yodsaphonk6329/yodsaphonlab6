5db4ae278226f52a302171f12f399a7bc7c2f3d5
yodsaphonk6329 <111995012+yodsaphonk6329@users.noreply.github.com>
2022-09-04 22:55:13 +0700

    yodsaphon

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+const { sequelize } = require('./models')
+
+const config = require('./config/config')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+require('./routes')(app)
+
+app.get('/status', function(req, res) {
+    res.send('')
+})
+
+
+let port = process.env.PORT || config.port;
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, function() {
+        console.log('server running on ' + port);
+    })
+})
---
+++ server/src/routes.js
@@ -0,0 +1,19 @@
+const UserController = require('./controllers/UserController')
+
+module.exports = (app) => {
+    app.post('/user',
+    UserController.create
+    )
+
+    app.put('/user/:userId',
+    UserController.put
+    )
+
+    app.delete('/user/:userId',
+    UserController.remove
+    )
+
+    app.get('/users',
+    UserController.index
+    )
+}
